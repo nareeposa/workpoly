a48616ca993325196edd2e7b520feb8f8b3ba741
nareeposa <nareeposa@gmail.com>
2024-12-24 13:10:30 +0700

    naree commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar'; 
+import Footer from './components/Footer';
+import Home from './Pages/Home';
+import Menu from './Pages/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main className="container mt-4">
+        <h1>Welcome To Restaurant Chiang Mai</h1>
+        <p>อาหารไทยอร่อย!</p>
+      </main>
+      <main className="container mt-4">
+          <Home/>
+      </main>
+      <main className="container mt-4">
+          <Menu/>
+      </main>
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Pages/Menu.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Menu() {
+    return(
+        <div class="card-group">
+        <div class="card">
+          <img src="https://static.thairath.co.th/media/B6FtNKtgSqRqbnNsbKFRA9Hw1ddaiN8vczDH5awGUi4JQ7XTjwF2YTlnGfAZTGUAcQDXv.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">ต้มยำกุ้ง</h5>
+            <p class="card-text">“ต้มยำกุ้ง” กับเอกลักษณ์ของรสชาติที่มีความสมดุลระหว่างรสเปรี้ยว เผ็ด เค็ม หวาน</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://img.wongnai.com/p/1920x0/2024/05/20/8b6b33ca3413465b80c0b37cc847ef9b.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">ผัดไทยกุ้ง</h5>
+            <p class="card-text">"ผัดไทยกุ้ง" เป็นหนึ่งในเมนูอาหารไทยยอดนิยมที่อยู่คู่ครัวคนไทยมานานแถมยังโด่งดังไปทั่วโลกอีกต่างหากค่ะ เป็นเมนูที่ทุกคนควรจะเรียนรู้วิธีทำไว้ เผื่อได้มีโอกาสโชว์ฝีมือทำอาหารไทยให้ใครได้ลองกิน อ่านต่อได้ที่ </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://www.maggi.co.th/sites/default/files/styles/home_stage_944_531/public/srh_recipes/9f922d8fe6344f4a8b641ebd716be4cd.jpg?h=4f5b30f1&itok=KRUdjO7X.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">"ข้าวผัดกุ้ง"</h5>
+            <p class="card-text">ข้าวผัด เป็นอาหารไทยประเภทข้าวผัด เป็นการนำข้าวสวยลงไปผัดคลุกกับซอส น้ำพริก หรือเครื่องปรุงรสต่าง ๆ เพื่อให้ได้รสชาติ</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Menu;
